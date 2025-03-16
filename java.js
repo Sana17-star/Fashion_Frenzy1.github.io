b0e8b2afcb7735ab8cbadd88719f79d22d248876
Sana <140684450+Sana17-star@users.noreply.github.com>
2025-03-16 11:35:06 +0500

    Update java.js

--- java.js
+++ java.js
@@ -1,9 +1,1 @@
-const form = document.getElementById('responsive-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
-});
